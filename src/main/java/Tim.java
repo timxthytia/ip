d3ecd9d26723e8c830595194cf0d5abd6a62da42
Timothy <timothytiayz@gmail.com>
2025-08-21 12:38:00 +0800

    Level-3

--- src/main/java/Tim.java
+++ src/main/java/Tim.java
@@ -2,7 +2,34 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Tim {
-    static ArrayList<String> tasks = new ArrayList<>(100);
+    // Nested Task class
+    static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        void markAsDone() {
+            this.isDone = true;
+        }
+
+        void markAsUndone() {
+            this.isDone = false;
+        }
+
+        String getStatusIcon() {
+            return (isDone ? "X" : " ");
+        }
+
+        @Override
+        public String toString() {
+            return "[" + getStatusIcon() + "] " + description;
+        }
+    }
+    static ArrayList<Task> tasks = new ArrayList<>(100);
     public static void main(String[] args) {
         /*
         String logo = " ____        _        \n"
@@ -16,15 +43,30 @@ public class Tim {
         while (true) {
             String input = sc.nextLine();
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:\n");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(" " + (i + 1) + ". " + tasks.get(i));
                 }
-
+            } else if (input.startsWith("mark ")){
+                String index = input.substring(5).trim();
+                int idx = Integer.parseInt(index);
+                Task done = tasks.get(idx - 1);
+                done.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" " + done);
+            } else if (input.startsWith("unmark ")){
+                String index = input.substring(7).trim();
+                int idx = Integer.parseInt(index);
+                Task undone = tasks.get(idx - 1);
+                undone.markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(" " + undone);
             } else if (input.equals("bye")){
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
             } else {
-                tasks.add(input);
+                Task newTask = new Task(input);
+                tasks.add(newTask);
                 System.out.println(" added: " + input);
             }
         }
